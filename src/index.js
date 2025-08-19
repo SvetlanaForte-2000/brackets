f4d76b1571e72d5171522fd5997d5305ef1fd1a5
SvetlanaForte-2000 <105925303+SvetlanaForte-2000@users.noreply.github.com>
2025-08-19 22:08:47 +0300

    feat: implement check function for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const pairs = {};
+
+  bracketsConfig.forEach(function buildPairs(pair) {
+    const [open, close] = pair;
+    pairs[close] = open;
+  });
+
+  let isValid = true;
+
+  str.split('').forEach(function processChar(char) {
+    if (!isValid) return;
+
+    const top = stack[stack.length - 1];
+
+    if (pairs[char] === char) {
+      if (top === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (!pairs[char]) {
+      stack.push(char);
+    } else if (top === pairs[char]) {
+      stack.pop();
+    } else {
+      isValid = false;
+    }
+  });
+
+  return isValid && stack.length === 0;
 };
